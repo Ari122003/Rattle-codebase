30f5450a5d43114ad2cbdf5147db12dd75c0c853
Aritra <aritraadhikary2003@gmail.com>
2023-02-18 16:05:49 +0530

    Thunder client collection of endpoints and error handling of authentication

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -4,31 +4,47 @@ const User = require("../Modules/User");
 const { body, validationResult } = require("express-validator");
 
 router.get(
-	"/",
+	"/CreateUser",
 	[
+		// credentials validators
+
 		body("email", "Enter a valid email").isEmail(),
 		body("name", "Enter a valid name").isLength({ min: 3 }),
 		body("password", "Password must have atleast 5 characters").isLength({
 			min: 5,
 		}),
 	],
+
+	//   If there is an error in credentials
+
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
+		// Check whether the user already exists or not
 
-		await User.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password,
-		}).then((user) => res.json(user)).catch(()=>{
-			res.send({
-				error:"This email is already avialable"
-			})
-		})
+		try {
+			let user = await User.findOne({ email: req.body.email });
 
-		
+			if (user) {
+				return res
+					.status(400)
+					.json({ error: "User with this email already exeits" });
+			}
+			//  create the user in database
+			user = await User.create({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password,
+			});
+
+			res.json(user);
+		} catch (error) {
+			//   if there is some error in the database
+			console.log(error.message);
+			res.status(500).send("SOME ERROR OCCURED");
+		}
 	}
 );
 
