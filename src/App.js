44d1ab56ae19607de02e72dfb1d38f25500e6397
Aritra <aritraadhikary2003@gmail.com>
2023-02-21 22:19:53 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter,Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 
 function App() {
 	return (
 		<>
+		<HashRouter>
+
 			<Navbar />
 
 			<Routes>
 				<Route exact path="/" element={<Home/>} />
 				<Route exact path="/about" element={<About/>} />
 			</Routes>
+		</HashRouter>
 		</>
 	);
 }
