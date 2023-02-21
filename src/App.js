21debbad2bfa67908fe3042eb1ef304bd6ee3733
Aritra <aritraadhikary2003@gmail.com>
2023-02-21 13:06:10 +0530

    Added bootstarp and tailwind , created components and react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 function App() {
-  return (
-   <h1>hello</h1>
-  );
+	return (
+		<>
+			<Navbar />
+
+			<Routes>
+				<Route exact path="/" element={<Home/>} />
+				<Route exact path="/about" element={<About/>} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
