dd5c6f636669efd90e5d7b4cd02247b556785b05
Aritra <aritraadhikary2003@gmail.com>
2023-06-21 22:02:43 +0530

    Fixing

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
  const connect = require("./database")
  const express = require('express')
  var cors = require('cors')
+ const path = require("path")
 
  connect()
 
@@ -9,6 +10,11 @@ const port = 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname,"./build")))
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./build/index.html"))
+})
 
 app.use("/api/auth", require("./Routes/auth"))
 app.use("/api/notes", require("./Routes/notes"))
