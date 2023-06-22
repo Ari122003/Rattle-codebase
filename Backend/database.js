50dd60a566883da2ed2da358e4847264b9f1c179
Aritra <aritraadhikary2003@gmail.com>
2023-06-22 23:15:06 +0530

    Fixing

--- Backend/database.js
+++ Backend/database.js
@@ -1,7 +1,8 @@
 const mong = require("mongoose");
 mong.set('strictQuery', true);
 
-const uri = "mongodb://127.0.0.1:27017/I-Notebook";
+// const uri = "mongodb://127.0.0.1:27017/I-Notebook";
+const uri = "mongodb+srv://Aritra:Aritra%402003@cluster0.kaovz5k.mongodb.net/";
 
 const connect = async () => {
 	await mong.connect(uri, () => {
