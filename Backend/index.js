9a83b1abcd8c0688707c8a0540e176628bd99fa8
Aritra <aritraadhikary2003@gmail.com>
2023-06-23 13:10:43 +0530

    Fixixng

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,19 @@
- const connect = require("./database")
- const express = require('express')
- var cors = require('cors')
- 
+require("dotenv").config();
+const connect = require("./database");
+const express = require("express");
+var cors = require("cors");
 
- connect()
+connect();
 
- const app = express()
-const port = 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-app.use("/api/auth", require("./Routes/auth"))
-app.use("/api/notes", require("./Routes/notes"))
+app.use("/api/auth", require("./Routes/auth"));
+app.use("/api/notes", require("./Routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
