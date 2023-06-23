2a0830df9d5f9f9009bc09e3450b454a6e9c9bff
Aritra <aritraadhikary2003@gmail.com>
2023-06-23 17:13:57 +0530

    Fixing

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,17 @@ var cors = require("cors");
 connect();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./Routes/auth"));
 app.use("/api/notes", require("./Routes/notes"));
+
+app.get("/", (req, res) => {
+	res.send("<h1>WORKING<h1>");
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port http://localhost:${port}`);
